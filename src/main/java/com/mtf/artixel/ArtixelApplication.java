778b844d3db97947fa1a167ef4325a0c347ee417
KHE <khe@meetingfan.com>
2026-02-10 12:49:58 +0900

    2026.02.10 : 프로젝트 커밋

--- src/main/java/com/mtf/artixel/ArtixelApplication.java
+++ src/main/java/com/mtf/artixel/ArtixelApplication.java
@@ -1,13 +1,16 @@
 package com.mtf.artixel;
 
+import com.mtf.artixel.config.SecurityConfig;
+import com.mtf.artixel.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.mtf.artixel", "com.mtf.artixel.config"})
+@Import({SecurityConfig.class, WebConfig.class})
 public class ArtixelApplication extends SpringBootServletInitializer {
 
 	// [중요] 톰캣이 실행될 때 이 메서드를 가장 먼저 찾아서 실행합니다.
